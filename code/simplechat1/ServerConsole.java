5b7c6e83bdd5d661dc43b0e53402a8948014f8a1
michelleoke <66561101+michelleoke@users.noreply.github.com>
2020-06-21 10:04:00 -0400

    Create ServerConsole.java

---
+++ code/simplechat1/ServerConsole.java
@@ -0,0 +1,109 @@
+import java.io.*;
+import client.*;
+import common.*;
+
+
+public class ServerConsole implements ChatIF 
+{
+  //Class variables *************************************************
+  
+  /**
+   * The default port to connect on.
+   */
+  final public static int DEFAULT_PORT = 5555;
+  
+  //Instance variables **********************************************
+  
+  /**
+   * The instance of the client that created this ConsoleChat.
+   */
+  EchoServer server;
+
+  
+  //Constructors ****************************************************
+
+  /**
+   * Constructs an instance of the ClientConsole UI.
+   *
+   * @param host The host to connect to.
+   * @param port The port to connect on.
+   */
+  public ServerConsole(int port) 
+  {
+  server = new EchoServer(port);
+    try 
+    {
+      server.listen();
+    } 
+    catch(IOException exception) 
+    {
+      System.out.println("Error: cannot listen for clients");
+      System.exit(1);
+    }
+  }
+
+  
+  //Instance methods ************************************************
+  
+  /**
+   * This method waits for input from the console.  Once it is 
+   * received, it sends it to the client's message handler.
+   */
+  public void accept() 
+  {
+    try
+    {
+      BufferedReader fromConsole = 
+        new BufferedReader(new InputStreamReader(System.in));
+      String message;
+
+      while (true) 
+      {
+        message = fromConsole.readLine();
+        client.handleMessageFromClientUI(message);
+      }
+    } 
+    catch (Exception ex) 
+    {
+      System.out.println
+        ("Unexpected error while reading from console!");
+    }
+  }
+
+  /**
+   * This method overrides the method in the ChatIF interface.  It
+   * displays a message onto the screen.
+   *
+   * @param message The string to be displayed.
+   */
+  public void display(String message) 
+  {
+    System.out.println("> " + message);
+  }
+
+  
+  //Class methods ***************************************************
+  
+  /**
+   * This method is responsible for the creation of the Client UI.
+   *
+   * @param args[0] The host to connect to.
+   */
+  public static void main(String[] args) 
+  {
+    String host = "";
+    int port = 0;  //The port number
+
+    try
+    {
+      host = args[0];
+    }
+    catch(ArrayIndexOutOfBoundsException e)
+    {
+      host = "localhost";
+    }
+    ClientConsole chat= new ClientConsole(host, DEFAULT_PORT);
+    chat.accept();  //Wait for console data
+  }
+}
+//End of ServerConsole class
